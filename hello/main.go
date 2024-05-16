e1f7e137e0bae0fc1989debcdce731d05312e808
Ryan Casey <rmcasey@Ryans-MacBook-Pro.local>
2024-05-16 07:45:38 -0400

    Iintal commit

---
+++ hello/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting := greet("vi")
+	fmt.Println(greeting)
+}
+
+type language string
+
+
+var phrasebook = map[language]string {
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",	   // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+        "ur": "ہیلو ",             // Urdu 
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
+}
---
+++ hello/main_internal_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestGreet_English(t *testing.T) {
+	lang := language("en")
+	want := "Hello world"
+	got := greet(lang)
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got %q", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le monde"
+	got := greet(lang)
+
+	if got != want {
+		//mark this test as failed
+		t.Errorf("expected: %q, got %q", want, got)
+	}
+
+}
+
+func TestGreet_Akkadian(t *testing.T) {
+	//Akkadian is not implemented yet!
+	lang := language("akk")
+	want := ""
+
+	got := greet(lang)
+
+	if got != want {
+		//mark this test as failed
+		t.Errorf("expected: %q, got %q", want, got)
+	}
+}
+
